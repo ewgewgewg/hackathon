d8b389c311783989f35d3bdb15a04397fbb7bccf
Andrew <ampaisner@gmail.com>
2019-09-18 20:19:58 -0400

    added permissions

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const content = document.querySelector('svg');
 content.parentNode.removeChild(content);
 
 document.title = `${1} ${document.title}`;
+localStorage.setItem('counter', '0');
+let x = parseInt(localStorage.getItem('counter'));
